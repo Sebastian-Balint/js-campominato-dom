0ce1127995d4096f74d1c46434445b5ecf09de30
Sebastian-Balint <sebastian.balint02@gmail.com>
2024-03-18 23:47:47 +0100

    add parte js

--- js/script.js
+++ js/script.js
@@ -8,6 +8,8 @@ const difficile = document.querySelector("option[value=difficile]");
 
 let lato = 10;
 let colMod = "col-facile";
+let arrayBombe = [];
+let arrayCampo = [];
 
 facile.addEventListener("click", function () {
   colMod = "col-facile";
@@ -27,6 +29,8 @@ difficile.addEventListener("click", function () {
 play.addEventListener("click", createGrid);
 
 function createGrid() {
+  createBombs();
+  console.log(arrayBombe, arrayCampo);
   row.innerHTML = ""; //reset
   let quadrato = lato ** 2;
   for (i = 0; i < quadrato; i++) {
@@ -35,22 +39,31 @@ function createGrid() {
     cell.className = colMod;
     cell.innerHTML = i + 1;
     cell.addEventListener("click", function () {
-      cell.classList.toggle("col-clicked");
+      cell.classList.add("col-clicked");
     });
   }
 }
 
-let arrayBombe = [];
-let arrayCampo = [];
-createBombs();
-console.log(arrayBombe, arrayCampo);
-
 function createBombs() {
-  for (i = 1; i < 17; i++) {
-    arrayBombe.push(`bomba numero ${i}`);
-  }
+  arrayBombe = [];
+  arrayCampo = [];
+  do {
+    let uniqueNumber = Math.floor(Math.random() * 100);
+    if (!arrayBombe.includes(uniqueNumber)) {
+      arrayBombe.push(uniqueNumber);
+    } else {
+    }
+  } while (arrayBombe.length < 16);
   let quadrato = lato ** 2;
   for (i = 1; i < quadrato + 1; i++) {
     arrayCampo.push(`cella numero ${i}`);
   }
 }
+
+// devo inserire le bombe
+// per inserire le bombe come faccio?
+// devo creare due array
+// i due array devo confrontarli
+// come faccio?
+// devo creare due array uguali di grandezza
+// nel array bombe pusho
